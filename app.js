307060c28c9d7e1cb8721a04861a76ffc3108086
alriyami123 <125434864+alriyami123@users.noreply.github.com>
2024-10-05 20:14:38 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 function sendEmail(event) {
-  event.preventDefault(); // Prevent form submission
+    event.preventDefault(); // Prevent form submission
 
-  const templateParams = {
-      to_email: document.getElementById('email').value, // User's email
-      message: document.getElementById('message').value, // User's message
-  };
+    const userEmail = document.getElementById('email').value; // Get user email
+    const templateParams = {
+        to_email: userEmail, // User's email
+        from_email: userEmail, // Set the sender email to the user's email
+        message: document.getElementById('message').value, // User's message
+    };
 
-  emailjs.send('Test', 'template_x0uf336', templateParams)
-      .then(function(response) {
-          showConfirmationMessage();
-      }, function(error) {
-          alert('Error sending email: ' + JSON.stringify(error));
-      });
+    emailjs.send('Test', 'template_x0uf336', templateParams)
+        .then(function(response) {
+            showConfirmationMessage();
+        }, function(error) {
+            alert('Error sending email: ' + JSON.stringify(error));
+        });
 }
