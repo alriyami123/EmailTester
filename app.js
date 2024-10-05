565d1ee62b1c13c3e1a098a540de913442079e95
alriyami123 <125434864+alriyami123@users.noreply.github.com>
2024-10-05 20:36:46 +0400

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ function sendEmail(event) {
 
     const userEmail = document.getElementById('email').value; // Get user email
     const messageContent = document.getElementById('message').value; // Get user's message
+
+    // Create an object with the template parameters
     const templateParams = {
         to_email: userEmail,    // User's email (recipient)
         from_email: userEmail,  // User's email (sender)
         message: messageContent, // User's message
     };
 
+    // Send email using EmailJS
     emailjs.send('Test', 'template_x0uf336', templateParams)
         .then(function(response) {
             showConfirmationMessage();
